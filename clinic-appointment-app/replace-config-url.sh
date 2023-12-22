6a1e4a0c794703a87d45e1fa71e8ba379bb103fe
TuqaMohamed <toka.m.mahdy@gmail.com>
2023-12-22 21:21:51 +0200

    front

--- clinic-appointment-app/replace-config-url.sh
+++ clinic-appointment-app/replace-config-url.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sed -i "s|\"API_URL\": \"default\"|\"API_URL\": \"$API_URL\"|g" src/config.json
+TMP_FILE=$(mktemp)
+sed "s|\"API_URL\": \"default\"|\"API_URL\": \"$API_URL\"|g" src/config.json > "$TMP_FILE"
+mv "$TMP_FILE" src/config.json
