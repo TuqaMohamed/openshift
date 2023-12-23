c18bb01086d7277a07893633ac29758c66d9514e
TuqaMohamed <toka.m.mahdy@gmail.com>
2023-12-24 01:37:02 +0200

    .

--- phase1/config/config.go
+++ phase1/config/config.go
@@ -12,12 +12,12 @@ var Client *mongo.Client
 var Context context.Context
 
 func ConnectDB(containerName string) {
-	clientOptions := options.Client().ApplyURI("https://my-mongo2-tuqamohamed-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com")
+	clientOptions := options.Client().ApplyURI("mongodb://mongo-container:27017/")
 	client, err := mongo.Connect(Context, clientOptions)
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	Client = client
-	fmt.Println("Connected to openshif MongoDB!")
+	fmt.Println("Connected to MongoDB!")
 }
