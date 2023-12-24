6dad680f04d72b8dcd381aaafdc2091b700f0421
TuqaMohamed <toka.m.mahdy@gmail.com>
2023-12-24 13:42:28 +0200

    another change in uri db

--- phase1/config/config.go
+++ phase1/config/config.go
@@ -15,7 +15,7 @@ var Context context.Context
 func ConnectDB(containerName string) {
 	dbURL:= os.Getenv("dbURL")
 	port := ":"+os.Getenv("PORT")
-	clientOptions := options.Client().ApplyURI("mongodb://"+dbURL+":"+port+"/")
+	clientOptions := options.Client().ApplyURI("mongodb://"+dbURL+port+"/")
 	client, err := mongo.Connect(Context, clientOptions)
 	if err != nil {
 		fmt.Println(err)
